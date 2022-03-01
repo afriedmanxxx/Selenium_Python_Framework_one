1101de79f057034a2612098aced8b9b024cfa5bd
afriedmanxxx <afriedmanxxx@gmail.com>
2022-03-01 15:12:54 -0800

    adding loginpage and locators.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from .pages.main_page import MainPage
+from .pages.locators import MainPageLocators
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -13,6 +15,5 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-    def go_to_login_page(browser):
-        login_link = browser.find_element_by_css_selector("#login_link")
-        login_link.click()
+
+
